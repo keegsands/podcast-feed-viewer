599beb80dfe5134d4373488c0f53361f1145ab6f
Keegan <keegsands@gmail.com>
2019-07-02 15:29:05 -0400

    Added summary and detail page
    - First major work for the podcast sample app.
    Small Fixes
    
    Updated to use different feed and fixed resulting issues in the UI
    Clean up warnings
    
    Fixed some warnings to the way states were loaded.
    Convert to XML
    
    Changed the feed reader to look for XML instead of JSON because most podcasts don't support the feed.json format yet.
    Flipped list order
    
    - Episodes now display in reverse order
    - Styling fixes for font and image borders

---
+++ src/components/summary/summary.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import axios from 'axios';
+import Detail from '../detail/detail';
+import convert from 'xml-js'
+
+
+export default class Summary extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            podcast: {items:[]},
+            loaded: false
+        };
+    }
+
+    
+    componentDidMount() {
+        var _this = this;
+        this.serverRequest =
+            axios
+                .get("http://localhost:3000/feed.xml")
+                .then(function (result) {
+                    _this.updateTheState(result.data);
+                })
+    }
+
+    updateTheState(feedXML){
+
+        const feedJSON = convert.xml2json(feedXML, {compact: true, spaces: 4});
+        this.setState({
+            podcast: JSON.parse(feedJSON),
+            loaded: true
+        });
+    }
+
+    componentWillUnmount() {
+        // this.serverRequest.abort();
+    }
+    render() {
+        let podcastTitle = '';
+        let podcastHome = '';
+        let podcastIcon = '';
+        let podcasts = [];
+        if (this.state && this.state.loaded) {
+            podcastTitle = this.state.podcast.rss.channel.title._text;
+            podcastHome = this.state.podcast.rss.channel.link._text;
+            podcastIcon = this.state.podcast.rss.channel['itunes:image']['_attributes']['href'];
+            podcasts = this.state.podcast.rss.channel.item;
+        }
+        if(!podcasts){
+            podcasts = [];
+        }
+
+        return (
+            <div><header><img width="300" alt="podcastIcon" src={podcastIcon} class="podcast-icon"></img><h2><a href={podcastHome}>{podcastTitle}</a></h2></header>
+                <ul>
+                    {podcasts.reverse().map((value, index) => {
+                        return <div key={index}><Detail podcast={value}/></div>
+                    })}
+                </ul>
+            </div>
+
+
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Summary from './components/summary/summary'
+import './index.css';
+
+ReactDOM.render(
+    <Summary/>,
+    document.getElementById('root')
+);
