2431831b625ce1419111501f26735cab22082f94
Keegan <keegsands@gmail.com>
2019-07-03 10:15:09 -0400

    Function Component Conversion
    Converted the main components over to be Function Components.

--- src/components/detail/detail.js
+++ src/components/detail/detail.js
@@ -1,34 +1,29 @@
 import React from 'react';
 
 
-export default class Detail extends React.Component {
-    
-    render() {
-        let podcastTitle = this.props.podcast.title._text;
-        if(!podcastTitle){
-            let publishDate = new Date(this.props.podcast.date_published);
-            podcastTitle = publishDate.toLocaleString('en-US');
-        }
-
-        let episodeLink = this.props.podcast.link._text;
-
-        let descriptionText = '';
-        if('content:encoded' in this.props.podcast){
-            descriptionText = this.props.podcast['content:encoded']._text;
-        }
-
-        if(!descriptionText){
-            if('_cdata' in this.props.podcast.description){
-                descriptionText = this.props.podcast.description._cdata;
-            }else{
-                descriptionText = this.props.podcast.description._text;
-            }
-        }
-        
-        
-
-        return (
-            <div><a href={episodeLink}><h3>{podcastTitle}</h3></a> <hr/><div dangerouslySetInnerHTML={{ __html: descriptionText }} /></div>
-        );
+const Detail = (props) => {
+    let podcastTitle = props.podcast.title._text;
+    if (!podcastTitle) {
+        let publishDate = new Date(props.podcast.date_published);
+        podcastTitle = publishDate.toLocaleString('en-US');
     }
+
+    let episodeLink = props.podcast.link._text;
+
+    let descriptionText = '';
+    if ('content:encoded' in props.podcast) {
+        descriptionText = props.podcast['content:encoded']._text;
+    }
+
+    if (!descriptionText) {
+        if ('_cdata' in props.podcast.description) {
+            descriptionText = props.podcast.description._cdata;
+        } else {
+            descriptionText = props.podcast.description._text;
+        }
+    }
+    return (
+        <div><a href={episodeLink}><h3>{podcastTitle}</h3></a> <hr /><div dangerouslySetInnerHTML={{ __html: descriptionText }} /></div>
+    );
 }
+export default Detail;
--- src/components/summary/summary.js
+++ src/components/summary/summary.js
@@ -1,67 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Detail from '../detail/detail';
 import convert from 'xml-js'
 
+const Summary = (props) => {
+    const [podcast, setPodcast] = useState({ items: [] });
+    const [loaded, setLoaded] = useState(false);
 
-export default class Summary extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            podcast: {items:[]},
-            loaded: false
-        };
+    useEffect(() => { loadFeed() }, []);
+
+    async function loadFeed() {
+        axios.get("http://localhost:3000/feed.xml")
+            .then(function (result) {
+                const feedJSON = convert.xml2json(result.data, { compact: true, spaces: 4 });
+                setPodcast(JSON.parse(feedJSON));
+                setLoaded(true);
+            })
+      }
+
+    let podcastTitle = '';
+    let podcastHome = '';
+    let podcastIcon = '';
+    let podcasts = [];
+    if (loaded) {
+        podcastTitle = podcast.rss.channel.title._text;
+        podcastHome = podcast.rss.channel.link._text;
+        podcastIcon = podcast.rss.channel['itunes:image']['_attributes']['href'];
+        podcasts = podcast.rss.channel.item;
+    }
+    if (!podcasts) {
+        podcasts = [];
     }
 
-    
-    componentDidMount() {
-        var _this = this;
-        this.serverRequest =
-            axios
-                .get("http://localhost:3000/feed.xml")
-                .then(function (result) {
-                    _this.updateTheState(result.data);
-                })
-    }
-
-    updateTheState(feedXML){
-
-        const feedJSON = convert.xml2json(feedXML, {compact: true, spaces: 4});
-        this.setState({
-            podcast: JSON.parse(feedJSON),
-            loaded: true
-        });
-    }
-
-    componentWillUnmount() {
-        // this.serverRequest.abort();
-    }
-    render() {
-        let podcastTitle = '';
-        let podcastHome = '';
-        let podcastIcon = '';
-        let podcasts = [];
-        if (this.state && this.state.loaded) {
-            podcastTitle = this.state.podcast.rss.channel.title._text;
-            podcastHome = this.state.podcast.rss.channel.link._text;
-            podcastIcon = this.state.podcast.rss.channel['itunes:image']['_attributes']['href'];
-            podcasts = this.state.podcast.rss.channel.item;
-        }
-        if(!podcasts){
-            podcasts = [];
-        }
-
-        return (
-            <div><header><img width="300" alt="podcastIcon" src={podcastIcon} className="podcast-icon"></img><h2><a href={podcastHome}>{podcastTitle}</a></h2></header>
-                <ul>
-                    {podcasts.reverse().map((value, index) => {
-                        return <div key={index}><Detail podcast={value}/></div>
-                    })}
-                </ul>
-            </div>
+    return (
+        <div><header><img width="300" alt="podcastIcon" src={podcastIcon} className="podcast-icon"></img><h2><a href={podcastHome}>{podcastTitle}</a></h2></header>
+            <ul>
+                {podcasts.reverse().map((value, index) => {
+                    return <div key={index}><Detail podcast={value} /></div>
+                })}
+            </ul>
+        </div>
 
 
-        );
-    }
+    );
 }
+export default Summary;
